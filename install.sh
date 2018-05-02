b5c8e44dab4f913015cc7705fcfbf26a4af4e922
loremzlimpa <loremzlimpa@gmail.com>
2018-05-02 11:43:11 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 sudo apt-get update
 sudo apt-get install python3 python3-pip
 
-sudo pip3 install python-telegram-bot --upgrade
-sudo pip3 install discord.py --upgrade
+sudo -h pip3 install python-telegram-bot --upgrade
+sudo -h pip3 install discord.py --upgrade
 
 sudo mkdir -p /opt/dstraTipBot
 
-sudo cp -prf ~/dstraTipBot/dstraDiscordTipBot
+sudo cp -prf ~/dstraTipBot/dstraDiscordTipBot /etc/init.d/dstraDiscordTipBot
 sudo chmod +x ~/etc/init.d/dstraDiscordTipBot
 sudo cp -prf ~/dstraTipBot/dstraDiscordTipBot.py /opt/dstraTipBot/dstraDiscordTipBot.py
 sudo cp -prf ~/dstraTipBot/pickledb.py /opt/dstraTipBot/pickledb.py
